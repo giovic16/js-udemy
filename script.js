e5663206218a91dda8266f82090d18038d627f4f
Giovana Victória <giovanavictoria887@gmail.com>
2022-04-08 10:40:18 -0300

    in production

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Olá Mundo!");
+
+var umNumero;
+var umNumero = 10; // Declara uma variável podendo atribuir um valor junto na sua criação
+
+let outroNumero = 5; // Declara uma variável com escopo de bloco
+
+const ola = "Olá Mundo!"; // Declara uma variável apenas para leitura. Após atribuir um valor, não será possível altera-lo
